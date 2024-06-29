b29509632714c598713ab5c425dc50ab82e3bf13
auroraptor <interestingbutno@yandex.ru>
2024-06-29 15:57:12 +0300

    chore: add global styles to apply font across the app

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import 'normalize.css';
 import App from "./App.tsx";
+import 'normalize.css';
+import './shared/styles/global.css';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
