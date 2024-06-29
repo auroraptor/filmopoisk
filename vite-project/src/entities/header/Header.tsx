3be412d6f8b4a83d85d64b9b0449c5c22e2d5b70
auroraptor <interestingbutno@yandex.ru>
2024-06-29 11:29:58 +0300

    feat: add avatar with SVG icon as background in Header component

--- vite-project/src/entities/header/Header.tsx
+++ vite-project/src/entities/header/Header.tsx
@@ -1,15 +1,20 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
+import { ButtonText } from './types';
+import { textLogo } from '../../shared/constants';
 
 function Header() {
 
-  const [buttonText, setButtonText] = useState('Войти')
+  const [buttonText, setButtonText] = useState<ButtonText>('Войти');
 
     return (
       <header className="header">
-        <Link to="/" className="logo">Фильмопоиск</Link>
+        <Link to="/" className="logo">{textLogo}</Link>
         <div className="userContainer">
+          <div className="avatarContainer">
+            <div className="avatar"/>
+          </div>
           <button className="button buttonSignin">{buttonText}</button>
         </div>
       </header>
---
+++ vite-project/src/entities/header/types.ts
@@ -0,0 +1,1 @@
+export type ButtonText = 'Войти' | 'Выйти';
